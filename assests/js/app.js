677dbfa629955ffbc6c2530ae6ee5bc6810498bf
Jon Eastin <picardncce@gmail.com>
2022-03-28 22:41:44 -0700

    made changes for next assignment

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,8 +1,7 @@
 const total = () => {
-
   // js html selectors
-  let t = document.querySelector('input[name="tier"]:checked').value;
-  let m = document.querySelector('input[name="month"]:checked').value;
+  let t = document.querySelector('input[name="tier"]:checked').value,
+  m = document.querySelector('input[name="month"]:checked').value;
 
   // calc of the selected values
   let result = Number(t) * Number(m);
@@ -12,7 +11,6 @@ const total = () => {
 }
 
 const showTotal = (evt) => {
-
   if (evt.target.id === 'price1') {
     document.getElementById('price1').innerText = '$0 / month';
   } else if (evt.target.id === 'price2') {
@@ -22,62 +20,78 @@ const showTotal = (evt) => {
   }
 }
 
-/*  
+const showOptions = (array) => {
+  let value = "<br>";
+  for (let i = 0; i < array.length; i++) {
+    value += array[i] + "<br>";
+  }
+  value = value + "- End of Options -"
+  return value;
+}
+
+/*
   Order object constructor
 */
-function Order(tier, month, username, mail, card) {
-
+function Order(tier, month, options, username, mail, card) {
   this.tier = tier;
   this.month = month;
+  this.options = options;
   this.username = username;
   this.mail = mail;
   this.card = card;
 }
 
-/* 
+/*
   Arrow function that uses the Constructor to build the object and send it to display
 */
 const createOrder = () => {
+  let tier = document.querySelector('input[name="tier"]:checked').value,
+  month = document.querySelector('input[name="month"]:checked').value,
+  options = [],
+  username = document.getElementById('username').value,
+  mail = document.getElementById('mail').value,
+  card = document.getElementById('card').value,
+  checkboxes = document.getElementsByName('option');
 
-  let tier = document.querySelector('input[name="tier"]:checked').value;
-  let month = document.querySelector('input[name="month"]:checked').value;
-  let username = document.getElementById('username').value;
-  let mail = document.getElementById('mail').value;
-  let card = document.getElementById('card').value;
-  let addOrder = new Order(tier, month, username, mail, card);
-  createOrderDisplay(addOrder);
+  for (let checkbox of checkboxes) {
+    if (checkbox.checked) {
+      options.push(checkbox.value);
+    }
+  }
+  let addOrder = new Order(tier, month, options, username, mail, card);
+  createDisplay(addOrder);
 }
 
-/* 
-  Setups up basic html and nodes for fomratting of the user reciept 
+/*
+  Setups up the container for the display portion of the order
 */
-const createOrderDisplay = (order) => {
-
-  let div = document.createElement('div');
-  let classAttr = document.createAttribute('class');
-  classAttr.value = "newOrder";
-  div.setAttributeNode(classAttr);
-  document.getElementById('display').appendChild(div);
-
-  div.innerHTML = "Your Tier: " + order.tier + "<br>" + 
-  "Your Month Subscription " + order.month + "<br>" + 
-  "Your Username: " + order.username + "<br>" + 
-  "Your E-mail: " + order.mail + "<br>" + 
-  "Your Card: " + order.card + "<br>";
+const createDisplay = (order) => {
+  let optionsList = showOptions(order.options)
+  document.getElementById('showOrder').innerHTML = 
+  "Your Tier - " + order.tier 
+  + "<br>" + 
+  "Your Month Subscription - " + order.month 
+  + "<br>" + 
+  "- Options - " + optionsList 
+  + "<br>" + 
+  "Your Username - " + order.username 
+  + "<br>" + 
+  "Your E-mail - " + order.mail 
+  + "<br>" + 
+  "Your Card - " + order.card + 
+  "<br>";
 }
 
-/* 
+/*
   Display object's for Order Views
 */
-const viewOrder = () => {
-
-  document.getElementById('makeOrder')
-    .addEventListener('click', function(event) {
-      createOrder();
-      event.preventDefault();
+const previewOrder = () => {
+  document.getElementById('makeOrder').addEventListener('click', function(event) {
+    createOrder();
+    event.preventDefault();
   })
 }
 
 window.onload = function() {
-  viewOrder();
+  previewOrder();
 }
